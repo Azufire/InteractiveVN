9b98f7f956a8935c4e2637f31e54c131f242246e
Azufire <49247681+Azufire@users.noreply.github.com>
2026-02-07 07:43:29 -0800

    updated readme, added hash function,

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 HTML/CSS project using Render and Node.js to host collaborate overlay elements.
+
+Framework details:
+Deploy: Node.js/Express
+Main Language: HTML/Javascript
+Main Libraries: JQuery, SubtleCrypto
+Database: AWS - RDS (MySQL)
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-
 app.get("/", (req, res) => res.sendFile(__dirname + '/index.html'));
 
 const server = app.listen(port, () => console.log(`App listening on port ${port}!`));
