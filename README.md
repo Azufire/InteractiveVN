5d69da35017b98eae3f5683bde5a3a1cc8a72c92
Azufire <49247681+Azufire@users.noreply.github.com>
2026-02-16 03:53:33 -0800

    minor refactoring, changed variable names for consistency, adjusted form inputs to fit feedback requests

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 HTML/CSS project using Render and Node.js to host collaborate overlay elements.
 
 Framework details:
+
 Deploy: Node.js/Express
 Main Language: HTML/Javascript
 Libraries: SubtleCrypto, Express-Validator, MySQL2, dotenv, Pug, cookie-parser
--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const {body, matchedData, validationResult} = require('express-validator');
 const cookieParser = require('cookie-parser')
 require('dotenv').config();
 const __dirname = import.meta.dirname;
-const app = express();
 const port = process.env.PORT || 3001;
+const app = express();
 //setup express render settings
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
@@ -17,6 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
 app.use(cookieParser(process.env.COOKIE));
+
+//setting up arrays for referencing answer values - moved up for easy changing
+const typeNames = ["BAD", "OK", "GOOD"];
+const typeVals = [process.env.VAL_BAD, process.env.VAL_OK, process.env.VAL_GOOD];
 
 //function to hash given sting (password) with SHA-256
     async function hashString(inputString) {
@@ -29,12 +33,12 @@ app.use(cookieParser(process.env.COOKIE));
     return hashHex;
     }
 
-//Parse login attempts - sanatize/validate inputs, hash and compare to password hash; create cookie and redirect if true, errormsg and return if false
+//Parse login attempts - sanatize/validate inputs, hash/compare password hashes; create cookie->redirect if true, errormsg and return if false
 app.post("/in", body("username").trim().notEmpty().escape(),
                 body("password").trim().notEmpty().escape(), async (req, res) => {
-    const result = validationResult(req);
+    const errors = validationResult(req);
     var errmsg = "";
-    if(result.isEmpty()) {
+    if(errors.isEmpty()) {
         const testPass = await hashString(matchedData(req).password);
         if(testPass === NotPassword) {
             res.cookie('username',matchedData(req).username, {httpOnly:true, signed: true});
@@ -43,25 +47,16 @@ app.post("/in", body("username").trim().notEmpty().escape(),
             errmsg = "Password incorrect!";
         }
     } else {
-        var longErr = false;
-        for(const item of result.array()){
-            if(item.path === "username"){
-                errmsg += "Invalid username ";
-                longErr = true;
-            } else if (item.path === "password") {
-                if (longErr){
-                    errmsg += "and password";
-                } else {
-                    errmsg += "Invalid password";
-                }
-
-            }
-        }
+        var results = errors.array();
+        errmsg = "Invalid ";
+        if (results.length == 2){
+            errmsg += "username and password"
+        } else (results[0].path === "username" ? errmsg += "username" : errmsg += "password");
     }
     return res.render("login", {msg: errmsg});
 });
 
-//redirect for main controls page: check for cookie, then query for table, then render view
+//redirect for main controls page: check for cookie -> query for table -> render view
 app.get('/main', async (req, res) => {
     const cookie = req.signedCookies.username;
     if (cookie == null){
@@ -71,30 +66,60 @@ app.get('/main', async (req, res) => {
     return res.render("main", {user: cookie, tableData: historyLog[0]});
 });
 
-//parse post requests change bar: send data to sql database, update bar
-app.post("/main", body("amount").trim().notEmpty().isNumeric().escape(), async (req, res) => {
+//parse post requests for new answers: send to sql database, update StreamElements bar
+app.post("/main", async (req, res) => {
     var errmsg = "";
-    const result = validationResult(req);
-    const changeVal = matchedData(req).amount;
-    if (result.isEmpty() && changeVal > 0){
+    var typeCode = req.body.changeType;
+    if (typeCode == -1) {
+        errmsg = "Please select a value from the dropdown!";
+    } else {
+        const changeType = typeNames[typeCode];
+        const changeVal = typeVals[typeCode];
         try {
+            const currTotal = await pool.query("SELECT total FROM History ORDER BY change_id DESC LIMIT 1;");
             await pool.query
-            ("INSERT INTO History (username, change_type, amount) VALUES (?, ?, ?)",
-                [req.signedCookies.username, req.body.changeType, changeVal]
+            ("INSERT INTO History (username, change_type, total) VALUES (?, ?, ?)",
+                [req.signedCookies.username, changeType, changeVal + currTotal]
             );
-            } catch (e) {
-                errmsg = "Something went wrong while connecting to the database...";
-                console.log(e);
-            }
-        } else {
-            errmsg = "Please use positive, nonzero values only!";
+            //the stream elements bar updating code would go here, i imagine
+        } catch (e) {
+            errmsg = "Something went wrong while connecting to the database...";
+            console.log(e);
+        }
     }
     const historyLog = await pool.query("SELECT * from History ORDER BY change_id DESC;");
     return res.render("main", 
         {user: req.signedCookies.username, tableData: historyLog[0], msg: errmsg});
 })
 
-//deploy express app with main page html
+//parse post requests to manually set bar value (bottom option): send data to sql database, update bar
+app.post("/set", body("total").trim().notEmpty().isNumeric().escape(), async (req, res) => {
+    var errmsg = "";
+    const errors = validationResult(req);
+    const changeVal = matchedData(req).total;
+    if (req.body.changeType != 4){
+        errmsg = "Select SET in the dropdown to confirm your change!";
+    } else if (errors.isEmpty() && changeVal >= 0){
+        try {
+            await pool.query
+            ("INSERT INTO History (username, change_type, total) VALUES (?, ?, ?)",
+                [req.signedCookies.username, "SET", changeVal]
+            );
+            errmsg = "Bar value has been manually set.";
+            //the stream elements bar updating code would go here, i imagine
+        } catch (e) {
+            errmsg = "Something went wrong while connecting to the database...";
+            console.log(e);
+        }
+        } else {
+            errmsg = "Please use non-negative values only!";
+    }
+    const historyLog = await pool.query("SELECT * from History ORDER BY change_id DESC;");
+    return res.render("main", 
+        {user: req.signedCookies.username, tableData: historyLog[0], msg: errmsg});
+})
+
+//deploy express app with login html
 app.get("/", (req, res) => res.render("login"));
 const server = app.listen(port, () => console.log(`App listening on port ${port}!`));
 server.keepAliveTimeout = 120 * 1000;
